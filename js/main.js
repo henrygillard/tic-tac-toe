8c41e2b6e0898a522cb28a8e2c784cf7ff779843
Henry Gillard <henry@MacBook-Pro.local>
2021-05-28 19:40:20 -0700

    Create Display Board HTML and CSS

---
+++ js/main.js
@@ -0,0 +1,18 @@
+/*----- constants -----*/
+
+
+/*----- app's state (variables) -----*/
+let displayBoard;
+
+
+/*----- cached element references -----*/
+
+
+/*----- event listeners -----*/
+
+
+/*----- functions -----*/
+function init() {
+    displayBoard = null;
+    render();
+}
